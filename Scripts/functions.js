5495b2d0f93d7331ceb9dbc4300f5948a432bed4
chris2011 <christian.lenz@gmx.net>
2012-02-21 14:39:23 +0000

    "First test."

---
+++ Scripts/functions.js
@@ -0,0 +1,34 @@
+(function()
+{
+//var init = function()
+//{
+//   var sceneObj = {
+//      WIDTH: 1024,
+//      HEIGHT: 768
+//   };
+
+//   var cameraAttr = {
+//      VIEW_ANGLE: 45,
+//      ASPECT: (sceneObj.WIDTH / sceneObj.HEIGHT),
+//      NEAR: 0.1,
+//      FAR: 10000
+//   };
+
+   // var canvasArea = document.getElementById("area");
+   var renderer = new THREE.WebGLRendderer();
+//   var camera = new THREE.PerspectiveCamera(cameraAttr.VIEW_ANGLE,
+//                                            cameraAttr.ASPECT,
+//                                            cameraAttr.NEAR,
+//                                            cameraAttr.FAR);
+//   var scene = new THREE.Scene();
+//
+//   camera.position.z = 300;
+//   renderer.setSize(sceneObj.WIDTH, sceneObj.HEIGHT);
+//   canvasArea.appendChild(renderer.domElement);
+}());
+// };
+
+//window.onload = function()
+//{
+//   init();
+//}
