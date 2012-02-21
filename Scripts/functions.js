e061923f1603f45dd5ef0ac86faba177458d6ab6
chris2011 <christian.lenz@gmx.net>
2012-02-21 16:28:18 +0000

    "Basic functions."

--- Scripts/functions.js
+++ Scripts/functions.js
@@ -1,34 +1,36 @@
-(function()
+//(function()
+//{
+var t = function()
 {
-//var init = function()
-//{
-//   var sceneObj = {
-//      WIDTH: 1024,
-//      HEIGHT: 768
-//   };
+   var sceneObj = {
+      WIDTH: 1024,
+      HEIGHT: 768
+   };
 
-//   var cameraAttr = {
-//      VIEW_ANGLE: 45,
-//      ASPECT: (sceneObj.WIDTH / sceneObj.HEIGHT),
-//      NEAR: 0.1,
-//      FAR: 10000
-//   };
+   var cameraAttr = {
+      VIEW_ANGLE: 45,
+      ASPECT: (sceneObj.WIDTH / sceneObj.HEIGHT),
+      NEAR: 0.1,
+      FAR: 10000
+   };
 
-   // var canvasArea = document.getElementById("area");
-   var renderer = new THREE.WebGLRendderer();
-//   var camera = new THREE.PerspectiveCamera(cameraAttr.VIEW_ANGLE,
-//                                            cameraAttr.ASPECT,
-//                                            cameraAttr.NEAR,
-//                                            cameraAttr.FAR);
-//   var scene = new THREE.Scene();
-//
-//   camera.position.z = 300;
-//   renderer.setSize(sceneObj.WIDTH, sceneObj.HEIGHT);
-//   canvasArea.appendChild(renderer.domElement);
-}());
-// };
+   var canvasArea = document.getElementById("area");
+   var renderer = new THREE.WebGLRenderer();
+   var camera = new THREE.PerspectiveCamera(cameraAttr.VIEW_ANGLE,
+                                            cameraAttr.ASPECT,
+                                            cameraAttr.NEAR,
+                                            cameraAttr.FAR);
+   var scene = new THREE.Scene();
 
-//window.onload = function()
-//{
-//   init();
-//}
+   camera.position.z = 300;
+   renderer.setSize(sceneObj.WIDTH, sceneObj.HEIGHT);
+   canvasArea.appendChild(renderer.domElement);
+
+   renderer.render(scene, camera);
+//}());
+};
+
+window.onload = function()
+{
+   t();
+};
